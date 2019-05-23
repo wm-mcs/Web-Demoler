2eb66a11f08d58330d0db2c07a84ab150ccdc2e6
Mauricio Costanzo <ventas@worldmaster.com.uy>
2019-05-23 15:30:29 -0300

    gh

--- resources/views/paginas/home/slider_texto_present.blade.php
+++ resources/views/paginas/home/slider_texto_present.blade.php
@@ -1,6 +1,6 @@
  <div class="col-lg-10 mx-auto">
       <h1 class="text-uppercase Slider_cabecera_caption_titulo">
-        <span class="slider-titulo-expecial"><strong>¿Buscas alhajas de calidad con una atención personalizada?</strong></span> 
+        <span class="slider-titulo-expecial"><strong>¿Buscas una empresa de demolición?</strong></span> 
       </h1>
       
 </div>
@@ -8,7 +8,7 @@
 <div class="col-lg-8 mx-auto">
   <h2 class="text-faded mb-5 Slider_cabecera_caption_sub_titulo text-color-primary">Tenemos lo que necesitas.</h2>
  
-  <a class="btn btn-primary btn-xl js-scroll-trigger Slider_cabecera_boton_contacto" href="#precios">VER CATALOGO</a>
+  <a class="btn btn-primary btn-xl js-scroll-trigger Slider_cabecera_boton_contacto" href="#precios">Contactar</a>
    {{-- <p class="text-faded my-4 text-color-primary" style="font-style:italic; font-weight: bold;">#WEBSDECALIDAD</p> --}}
 </div>
          
