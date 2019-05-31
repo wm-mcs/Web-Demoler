cde347a48387b85f20a75f855e53c64b4ff23d0d
Mauricio Costanzo <ventas@worldmaster.com.uy>
2019-05-31 17:32:37 -0300

    hj

--- resources/views/paginas/home/home_boton_contactar_whatsapp.blade.php
+++ resources/views/paginas/home/home_boton_contactar_whatsapp.blade.php
@@ -1,6 +1,6 @@
-<div class="get_width_100 flex-row-column bg-primary py-4">
+<div class="get_width_100 flex-row-column  py-4">
             <a class="mt-5" href="https://api.whatsapp.com/send?phone={{$Empresa->numero_whatsapp_ya_arreglado}}&text=Hola!">
-            <div class="boton-simple-blanco-whatsapp">
+            <div class="boton-simple">
              Contáctame ahora <i class="fab fa-whatsapp"></i>
             </div>
           </a>
