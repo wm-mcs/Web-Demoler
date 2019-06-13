14eb9cfdeb3618f76ea2093a1554e1becc622195
Mauricio Costanzo <ventas@worldmaster.com.uy>
2019-06-13 16:16:01 -0300

    fg

--- resources/views/paginas/home/home.blade.php
+++ resources/views/paginas/home/home.blade.php
@@ -20,7 +20,7 @@
         
       <img data-src="{{url()}}/imagenes/Home/1.jpg" class="get_width_100">  
       @include('paginas.home.home_service')   
-      <img data-src="{{url()}}/imagenes/Home/2.jpg" class="get_width_100">   
+      {{-- <img data-src="{{url()}}/imagenes/Home/2.jpg" class="get_width_100">   --}} 
 @stop      
 
 
