60cbada862b7311a9c3053bdf2c1ed5dfda56775
Mauricio Costanzo <ventas@worldmaster.com.uy>
2019-06-18 13:24:09 -0300

    ok

--- resources/views/layouts/user_layout/navbar/navbar.blade.php
+++ resources/views/layouts/user_layout/navbar/navbar.blade.php
@@ -9,12 +9,12 @@
 
       <ul class="navbar-nav ml-auto"> 
 
-          <li class="nav-item">
+         {{--  <li class="nav-item">
               <a class="nav-link " href="https://nicolas.worldmaster.com.uy/Publicacion/Significado-de-las-Piedras-Energ%C3%A9ticas-m%C3%A1s-utilizadas-en-joyer%C3%ADa/1">SIGNIFICADOS</a>
             </li> 
              <li class="nav-item">
               <a class="nav-link " href="https://nicolas.worldmaster.com.uy/Publicacion/Como-cuidar-una-joya/2">CUIDADOS</a>
-            </li>        
+            </li>      --}}   
 
         <li class="nav-item">
           <a class="nav-link js-scroll-trigger" href="#contact">CONTACTO</a>
--- resources/views/paginas/noticias/noticias_lista_individual.blade.php
+++ resources/views/paginas/noticias/noticias_lista_individual.blade.php
@@ -1,12 +1,24 @@
         <div class="post-preview">
-          <a href=" {{$Noticia->route}}">
+         
             <h2 class="post-title">
               {{$Noticia->name}} 
             </h2>
             <h3 class="post-subtitle">
                 {{$Noticia->sub_name}} 
             </h3>
-          </a>
+
+             <div class="flex-row-column get_width_100">
+              <div class="contiene-video">
+                  <div class="video-responsive">
+                     <iframe  src="https://www.youtube.com/embed/{{$Noticia->url_video}}" frameborder="0" allowfullscreen="allowfullscreen"></iframe>
+                  </div>
+
+                  <div class="post-img-texto">
+                    {{$Noticia->description}} 
+                  </div>
+              </div>
+           </div>
+         
           <p class="post-meta">Publicado por
             <a href="#">{{$Empresa->name}}</a> el
              {{$Noticia->created_at->format('d,m,y')}} 
