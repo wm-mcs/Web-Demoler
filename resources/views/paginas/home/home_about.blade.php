de80296d2627ce493d266107dc8c99ce266550ea
Mauricio Costanzo <ventas@worldmaster.com.uy>
2019-07-15 18:10:25 -0300

    fg

--- resources/views/paginas/home/home_about.blade.php
+++ resources/views/paginas/home/home_about.blade.php
@@ -9,7 +9,7 @@
             <h2 class="section-heading text-white">¿Quienes somos? </h2>
            
             <hr class="light my-4">
-            <p class="text-white mb-4">Somos una empresa con vasta experiencia en el rubro y en constante crecimiento; nos encargamos de todo tipo de trabajos que implique el uso de mano de obra calificada y maquinaria apta para el mismo (demolición de todo tipo finca incluso con peligro de derrumbe,  excavaciones, movimientos de tierra, etc.) Estamos siempre dispuestos y a las órdenes en brindarte el mejor trabajo de calidad para tus necesidades y satisfacción.
+            <p class="text-white mb-4">Somos una empresa con vasta experiencia en el rubro y en constante crecimiento; nos encargamos de todo tipo de trabajos que implique el uso de mano de obra calificada y maquinaria apta para el mismo (demoliciones de todo tipo finca incluso con peligro de derrumbe,  excavaciones, movimientos de tierra, etc.) Estamos siempre dispuestos y a las órdenes en brindarte el mejor trabajo de calidad para tus necesidades y satisfacción.
             </p>
 
         
