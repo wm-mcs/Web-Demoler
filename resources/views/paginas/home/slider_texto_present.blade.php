1e9f258ee1e34cde7c3f9fce3c22f5b6b34cbcaa
Mauricio Costanzo <ventas@worldmaster.com.uy>
2019-08-16 12:38:33 -0300

    df

--- resources/views/paginas/home/slider_texto_present.blade.php
+++ resources/views/paginas/home/slider_texto_present.blade.php
@@ -8,7 +8,7 @@
 <div class="col-lg-8 mx-auto">
   <h2 class="text-faded mb-5 Slider_cabecera_caption_sub_titulo text-color-primary">Tenemos lo que necesitas.</h2>
  
-  <a class="btn btn-primary btn-xl js-scroll-trigger Slider_cabecera_boton_contacto" href="#precios">Contactar</a>
+  <a class="btn btn-primary btn-xl js-scroll-trigger Slider_cabecera_boton_contacto" href="#contact">Contactar</a>
    {{-- <p class="text-faded my-4 text-color-primary" style="font-style:italic; font-weight: bold;">#WEBSDECALIDAD</p> --}}
 </div>
          
